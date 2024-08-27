54354c462e0d92463af2e48aad382c53fdd1e462
pedro-kaliel <pedrokaliel@being.com.br>
2024-08-27 17:36:42 -0300

    Add files via upload
    *formulário em processo de finalização
    *card informativo finalizado
    *Button de contato removido do cabeçalho
    *detalhes de borda adicionados na section de vídeo

--- landing_page_html-css/script.js
+++ landing_page_html-css/script.js
@@ -1,17 +1,18 @@
+window.addEventListener("scroll", function(){
+    var header= document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
+
 function cadastro(){
     var nome= form.nome;
     var email= form.email;
     var empresa= form.emp;
 
     if((nome.value=="")||(email.value)(empresa.value)){
-        alert("preencha os campos obrigatórios");
+        alert("preencha os campos obrigatórios"); 
     }else{
         alert("cadastro bem sucedido")
     }
 }
 
-window.addEventListener("scroll", function(){
-    var header= document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
-})
 
